a0aa9af9b780f396540476bc9e7f806eecf5d3dd
Maria José Soto <A01705840@tec.mx>
2024-04-04 14:57:35 -0600

    Fix - lead.model poner constructores

--- Avance de proyecto/LeadSales/models/lead.model.js
+++ Avance de proyecto/LeadSales/models/lead.model.js
@@ -1,1 +1,11 @@
 const db = require('../util/database');
+const bcrypt = require('bcryptjs');
+
+module.exports = class Version {
+    constructor(mi_idVersion, mi_iduser, mi_fecha, mi_nombreVersion) {
+        this.IDVersion = mi_idVersion;
+        this.IDUser = mi_iduser;
+        this.FechaCreacion = mi_fecha;
+        this.NombreVersion = mi_nombreVersion;
+    }
+}
