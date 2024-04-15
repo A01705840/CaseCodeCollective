ccaf6c8ff59a377e7a2773127666a21a4b23bf63
Mauricio Salas Hernández <A01710309@tec.mx>
2024-04-15 12:01:31 -0600

    Db leadsales2

--- Avance_de_proyecto/LeadSales/util/database.js
+++ Avance_de_proyecto/LeadSales/util/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'leadsalesdb1',
+    database: 'leadsalesdb2',
     password: '',
 });
 module.exports = pool.promise();
