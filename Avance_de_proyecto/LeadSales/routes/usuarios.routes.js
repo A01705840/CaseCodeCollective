ccd18be612bd8bbe5b647ae8da7b57575ad8ff65
Maria José Soto <A01705840@tec.mx>
2024-04-14 18:44:43 -0600

    FEAT - Añadir requisito isAuth en usuario rutas

--- Avance_de_proyecto/LeadSales/routes/usuarios.routes.js
+++ Avance_de_proyecto/LeadSales/routes/usuarios.routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const usuariosController= require('../controllers/usuarios.controller');
 
+const isAuth = require('../util/isAuth');
+
 
 router.get('/login', usuariosController.get_login);
 
-router.get('/signup', usuariosController.get_signup);
+router.get('/signup', isAuth, usuariosController.get_signup);
 
-router.post('/signup', usuariosController.post_signup);
+router.post('/signup', isAuth, usuariosController.post_signup);
 
 module.exports = router;
