4cbc15887fd1ee34f103d92619842e65a93ff26b
Maria José Soto <A01705840@tec.mx>
2024-04-15 00:01:06 -0600

    FEAT - Añadir canViewLeads para tabla de Leads funciona solo en vista

--- Avance_de_proyecto/LeadSales/routes/leads.routes.js
+++ Avance_de_proyecto/LeadSales/routes/leads.routes.js
@@ -4,17 +4,18 @@ const router = express.Router();
 const isAuth = require('../util/isAuth');
 const canEdit = require('../util/canEdit');
 const canViewRoles = require('../util/canViewRoles');
+const canViewLeads = require('../util/canViewLeads');
 
 const LeadsController = require('../controllers/leads.controller');
 const VersionController = require('../controllers/version.controller');
 
 router.get('/Analitica', isAuth, LeadsController.get_analitica);
-router.get('/', isAuth, canViewRoles, LeadsController.get_root);
+router.get('/', isAuth, canViewRoles, canViewLeads, LeadsController.get_root);
 
 router.get('/Historial', isAuth, VersionController.get_historial);
 router.post('/Historial', isAuth, VersionController.post_historial);
 
-router.get('/Leads', isAuth, LeadsController.get_leads);
+router.get('/Leads', canViewLeads, isAuth, LeadsController.get_leads);
 router.post('/Leads/eliminar', isAuth, LeadsController.post_eliminar_lead);
 
 router.get('/modificar/:id', isAuth, LeadsController.get_modificar_lead);
