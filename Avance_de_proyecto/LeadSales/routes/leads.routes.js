f588250c06d9cae8025b796a398c8c4c320e7f27
Maria José Soto <A01705840@tec.mx>
2024-04-15 07:35:51 -0600

    FEAT - Añadir canViewGráficas

--- Avance_de_proyecto/LeadSales/routes/leads.routes.js
+++ Avance_de_proyecto/LeadSales/routes/leads.routes.js
@@ -6,6 +6,7 @@ const canEdit = require('../util/canEdit');
 const canViewRoles = require('../util/canViewRoles');
 const canViewLeads = require('../util/canViewLeads');
 const canViewHistorial = require('../util/canViewHistorial');
+const canViewGraficas = require('../util/canViewGraficas');
 
 const LeadsController = require('../controllers/leads.controller');
 const VersionController = require('../controllers/version.controller');
@@ -13,15 +14,15 @@ const VersionController = require('../controllers/version.controller');
 router.get('/Analitica', isAuth, LeadsController.get_analitica);
 router.get('/', isAuth, canViewRoles, canViewLeads, LeadsController.get_root);
 
-router.get('/Historial', isAuth, VersionController.get_historial);
-router.post('/Historial', isAuth, VersionController.post_historial);
+router.get('/Historial', isAuth, canViewHistorial, VersionController.get_historial);
+router.post('/Historial', isAuth, canViewHistorial, VersionController.post_historial);
 
 router.get('/Leads', canViewLeads, isAuth, LeadsController.get_leads);
-router.post('/Leads/eliminar', isAuth, LeadsController.post_eliminar_lead);
+router.post('/Leads/eliminar', isAuth, canViewLeads, LeadsController.post_eliminar_lead);
 
-router.get('/modificar/:id', isAuth, LeadsController.get_modificar_lead);
-router.post('/modificar', isAuth, LeadsController.post_modificar_lead);
+router.get('/modificar/:id', isAuth, canViewLeads, LeadsController.get_modificar_lead);
+router.post('/modificar', isAuth, canViewLeads, LeadsController.post_modificar_lead);
 
-router.post('/Analitica', isAuth, LeadsController.postAnalitica);
+router.post('/Analitica', canViewGraficas, isAuth, LeadsController.postAnalitica);
 
 module.exports = router;
