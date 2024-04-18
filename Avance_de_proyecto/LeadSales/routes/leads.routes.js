f024635864ba6bafeb568ce7d854443ea6d30284
Galo del Rio <A01710791@tec.mx>
2024-04-18 13:45:24 -0600

    nuevas rutas

---
+++ Avance_de_proyecto/LeadSales/routes/leads.routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const router = express.Router();
+const isAuth = require('../util/isAuth');
+const canEdit = require('../util/canEdit');
+
+const LeadsController = require('../controllers/leads.controller');
+const VersionController = require('../controllers/version.controller');
+
+router.get('/Analitica', LeadsController.get_analitica);
+router.get('/', LeadsController.get_root);
+
+router.get('/Historial', VersionController.get_historial);
+router.post('/Historial', VersionController.post_historial);
+
+router.get('/Leads', LeadsController.get_leads);
+router.post('/Leads/eliminar', LeadsController.post_eliminar_lead);
+
+router.get('/modificar/:id', LeadsController.get_modificar_lead);
+router.post('/modificar', LeadsController.post_modificar_lead);
+
+router.post('/Analitica', LeadsController.postAnalitica);
+
+module.exports = router;
