00a8b4f9a92f8541c1f4df41f2bea8cd302950ac
Maria José Soto <A01705840@tec.mx>
2024-04-18 14:56:34 -0600

    FEAT .env

--- Avance_de_proyecto/LeadSales/util/database.js
+++ Avance_de_proyecto/LeadSales/util/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'leadsalesdb3',
-    password: '',
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
 });
 module.exports = pool.promise();
