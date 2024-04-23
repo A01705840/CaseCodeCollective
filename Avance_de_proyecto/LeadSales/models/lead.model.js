1cf53115a85a32510431989bcfb603b9f4426084
Tony <111481792+Bowist27@users.noreply.github.com>
2024-04-23 11:27:28 -0600

    FIX STYLE TABLES

--- Avance_de_proyecto/LeadSales/models/lead.model.js
+++ Avance_de_proyecto/LeadSales/models/lead.model.js
@@ -36,7 +36,7 @@ module.exports = class Lead {
         [mi_asignado_a,mi_telefono,mi_nombreLead,mi_FechaPrimerMensaje,mi_Embudo,mi_Etapa,mi_Status,mi_Archivado,mi_CreadoManual])
     }  
     static max(){
-        return db.execute(`SELECT MAX(IDLead) FROM leads;`)
+        return db.execute('SELECT MAX(IDLead) FROM leads;')
     }  
         static async fetchAll() {
             console.log(db.execute('SELECT * FROM leads'))
@@ -76,123 +76,121 @@ module.exports = class Lead {
             default:
                 throw new Error('Invalid range');
         }
-    
         const query = `
-            SELECT DATE(FechaPrimerMensaje) as Fecha, COUNT(*) as CantidadLeads 
-            FROM leads 
-            WHERE FechaPrimerMensaje >= ? AND FechaPrimerMensaje < ?
-            GROUP BY ${groupBy}(FechaPrimerMensaje)
-        `;
-        return await db.execute(query, [startDate, endDate]);
-    }
-    
-    static async obtenerCantidadLeads() {
-        const [rows] = await db.execute('SELECT COUNT(*) AS cantidad FROM leads');
-        return rows[0].cantidad;
-    }
-    
-    static async obtenerCantidadLeadsOrganicos() {
-        const [rows] = await db.execute('SELECT COUNT(*) AS cantidad FROM leads WHERE CreadoManual = 0');
-        return rows[0].cantidad;
-    }
+        SELECT DATE(FechaPrimerMensaje) as Fecha, COUNT(*) as CantidadLeads 
+        FROM leads 
+        WHERE FechaPrimerMensaje >= ? AND FechaPrimerMensaje < ?
+        GROUP BY ${groupBy}(FechaPrimerMensaje)
+    `;
+    return await db.execute(query, [startDate, endDate]);
+}
 
-    static async obtenerCantidadLeadsEmbudos() {
-        return db.execute('SELECT Embudo, COUNT(*) AS TotalLeads FROM leads GROUP BY Embudo')
-    }
+static async obtenerCantidadLeads() {
+    const [rows] = await db.execute('SELECT COUNT(*) AS cantidad FROM leads');
+    return rows[0].cantidad;
+}
 
-    static async fetchLeadsPorAgente(rangeAgent) {
-        const endDate = new Date(2023, 0, 1); // Fecha actual
-        let startDate = new Date(endDate); // Crea una copia de endDate
-        let groupBy;
-        console.log(rangeAgent);
-    
-        switch (parseInt(rangeAgent)) {
-            case 1:
-                startDate.setDate(endDate.getDate() - 7); // Una semana antes de la fecha actual
-                groupBy = 'DAY';
-                break;
-            case 2:
-                startDate.setMonth(endDate.getMonth() - 1); // Un mes antes de la fecha actual
-                groupBy = 'DAY';
-                break;
-            default:
-                throw new Error('Invalid range');
-        }
-    
-        const query = `
-            SELECT DATE(FechaPrimerMensaje) AS Fecha, asignado_a AS Agente, COUNT(*) as CantidadLeads 
-            FROM leads 
-            WHERE FechaPrimerMensaje >= ? AND FechaPrimerMensaje < ?
-            GROUP BY Fecha, asignado_a, ${groupBy}(FechaPrimerMensaje)
-        `;
-        return await db.execute(query, [startDate, endDate]);
-    }
+static async obtenerCantidadLeadsOrganicos() {
+    const [rows] = await db.execute('SELECT COUNT(*) AS cantidad FROM leads WHERE CreadoManual = 0');
+    return rows[0].cantidad;
+}
 
-    static async fetchLeadsPorAgenteAgrupadosPorMes(rangeAgent) {
-        const endDate = new Date(2023, 0, 1); // Fecha actual
-        let startDate = new Date(endDate); // Crea una copia de endDate
-    
-        switch (parseInt(rangeAgent)) {
-            case 3:
-                startDate.setMonth(endDate.getMonth() - 6); // Seis meses antes de la fecha actual
-                break;
-            case 4:
-                startDate.setFullYear(endDate.getFullYear() - 1); // Un año antes de la fecha actual
-                break;
-            default:
-                throw new Error('Invalid range');
-        }
-    
-        const query = `
-            SELECT DATE_FORMAT(FechaPrimerMensaje, '%Y-%m') AS Fecha, asignado_a AS Agente, COUNT(*) as CantidadLeads 
-            FROM leads
-            WHERE FechaPrimerMensaje >= ? AND FechaPrimerMensaje < ?
-            GROUP BY Fecha, asignado_a
-            ORDER BY Fecha, asignado_a
-        `;
-        const [rows] = await db.execute(query, [startDate, endDate]);
-    
-        // Crear un conjunto de todas las fechas y agentes únicos
-        let fechas = [...new Set(rows.map(row => row.Fecha))];
-        let agentes = [...new Set(rows.map(row => row.Agente))];
-    
-        // Crear un conjunto de datos para cada agente
-        let datasets = agentes.map(agente => {
-            let datos = fechas.map(fecha => {
-                // Encontrar la fila correspondiente a esta fecha y agente
-                let row = rows.find(row => row.Fecha === fecha && row.Agente === agente);
-                // Si se encuentra una fila, usar la cantidad de leads, de lo contrario usar 0
-                return row ? row.CantidadLeads : 0;
-            });
-            return { agente, datos };
-        });
-        
-        return { startDate, endDate, fechas, datasets };
-    }
-    
-    static fetchOne(NombreLead) {
-        return db.execute('Select * FROM usuario WHERE NombreLead = ?', [NombreLead]);
-    }
-    static fetchOneLeadbyid(id) {
-        return db.execute('Select * FROM leads WHERE IDLead = ?', [id]);
-    }
+static async obtenerCantidadLeadsEmbudos() {
+    return db.execute('SELECT Embudo, COUNT(*) AS TotalLeads FROM leads GROUP BY Embudo')
+}
 
-    static update(data) {
-        console.log('update');
-        console.log(data);
-        return db.execute('UPDATE leads SET asignado_a = ?, Telefono = ?, NombreLead = ?, FechaPrimerMensaje = ?, Embudo = ?, Etapa = ?, Status = ?, Archivado = ?, CreadoManual = ? WHERE IDLead = ?',
-            [data.asignado_a, data.telefono, data.nombre, data.fecha, data.embudo, data.etapa, data.status, data.archivado, data.creadomanual, data.id]);
-    }
+static async fetchLeadsPorAgente(rangeAgent) {
+    const endDate = new Date(2023, 0, 1); // Fecha actual
+    let startDate = new Date(endDate); // Crea una copia de endDate
+    let groupBy;
+    console.log(rangeAgent);
 
-    static crear(data) {
-        return db.execute('INSERT INTO leads (asignado_a, Telefono, NombreLead, FechaPrimerMensaje, Embudo, Etapa, Status, Archivado, CreadoManual) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-            [data.asignado_a, data.telefono, data.nombre, data.fecha, data.embudo, data.etapa, data.status, data.archivado, data.creadomanual]);
+    switch (parseInt(rangeAgent)) {
+        case 1:
+            startDate.setDate(endDate.getDate() - 7); // Una semana antes de la fecha actual
+            groupBy = 'DAY';
+            break;
+        case 2:
+            startDate.setMonth(endDate.getMonth() - 1); // Un mes antes de la fecha actual
+            groupBy = 'DAY';
+            break;
+        default:
+            throw new Error('Invalid range');
     }
+    const query = `
+    SELECT DATE(FechaPrimerMensaje) AS Fecha, asignado_a AS Agente, COUNT(*) as CantidadLeads 
+    FROM leads 
+    WHERE FechaPrimerMensaje >= ? AND FechaPrimerMensaje < ?
+    GROUP BY Fecha, asignado_a, ${groupBy}(FechaPrimerMensaje)
+`;
+return await db.execute(query, [startDate, endDate]);
+}
 
-    static async eliminar(id) {
-        await db.execute('DELETE FROM version_almacena_leads WHERE IDLead = ?', [id]);         
-        return db.execute('DELETE FROM leads WHERE IDLead = ?', [id]);
-    }
+static async fetchLeadsPorAgenteAgrupadosPorMes(rangeAgent) {
+const endDate = new Date(2023, 0, 1); // Fecha actual
+let startDate = new Date(endDate); // Crea una copia de endDate
 
-    
+switch (parseInt(rangeAgent)) {
+    case 3:
+        startDate.setMonth(endDate.getMonth() - 6); // Seis meses antes de la fecha actual
+        break;
+    case 4:
+        startDate.setFullYear(endDate.getFullYear() - 1); // Un año antes de la fecha actual
+        break;
+    default:
+        throw new Error('Invalid range');
+}
+
+const query = `
+    SELECT DATE_FORMAT(FechaPrimerMensaje, '%Y-%m') AS Fecha, asignado_a AS Agente, COUNT(*) as CantidadLeads 
+    FROM leads
+    WHERE FechaPrimerMensaje >= ? AND FechaPrimerMensaje < ?
+    GROUP BY Fecha, asignado_a
+    ORDER BY Fecha, asignado_a
+`;
+const [rows] = await db.execute(query, [startDate, endDate]);
+
+// Crear un conjunto de todas las fechas y agentes únicos
+let fechas = [...new Set(rows.map(row => row.Fecha))];
+let agentes = [...new Set(rows.map(row => row.Agente))];
+
+// Crear un conjunto de datos para cada agente
+let datasets = agentes.map(agente => {
+    let datos = fechas.map(fecha => {
+        // Encontrar la fila correspondiente a esta fecha y agente
+        let row = rows.find(row => row.Fecha === fecha && row.Agente === agente);
+ // Si se encuentra una fila, usar la cantidad de leads, de lo contrario usar 0
+ return row ? row.CantidadLeads : 0;
+});
+return { agente, datos };
+});
+
+return { startDate, endDate, fechas, datasets };
+}
+
+static fetchOne(NombreLead) {
+return db.execute('Select * FROM usuario WHERE NombreLead = ?', [NombreLead]);
+}
+static fetchOneLeadbyid(id) {
+return db.execute('Select * FROM leads WHERE IDLead = ?', [id]);
+}
+
+static update(data) {
+console.log('update');
+console.log(data);
+return db.execute('UPDATE leads SET asignado_a = ?, Telefono = ?, NombreLead = ?, FechaPrimerMensaje = ?, Embudo = ?, Etapa = ?, Status = ?, Archivado = ?, CreadoManual = ? WHERE IDLead = ?',
+[data.asignado_a, data.telefono, data.nombre, data.fecha, data.embudo, data.etapa, data.status, data.archivado, data.creadomanual, data.id]);
+}
+
+static crear(data) {
+return db.execute('INSERT INTO leads (asignado_a, Telefono, NombreLead, FechaPrimerMensaje, Embudo, Etapa, Status, Archivado, CreadoManual) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+[data.asignado_a, data.telefono, data.nombre, data.fecha, data.embudo, data.etapa, data.status, data.archivado, data.creadomanual]);
+}
+
+static async eliminar(id) {
+await db.execute('DELETE FROM version_almacena_leads WHERE IDLead = ?', [id]);         
+return db.execute('DELETE FROM leads WHERE IDLead = ?', [id]);
+    }
+
+    
 }
